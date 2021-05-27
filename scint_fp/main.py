29bd7a7163ec562a3d7e270343f1c7044f5636ce
beth182 <bethany.saunders@pgr.reading.ac.uk>
2021-05-27 18:21:37 +0100

    create fp

--- scint_fp/main.py
+++ scint_fp/main.py
@@ -7,9 +7,7 @@ from scint_fp.functions import estimate_z0, wx_u_v_components, retrieve_var, wx_
 import numpy as np
 import copy
 
-
 out_dir = 'C:/Users/beths/Desktop/LANDING/fp_raster_tests/'
-
 
 # files should be WX from new_scint_data, L2
 # and scint data from new_scint_data, L1
@@ -21,10 +19,10 @@ rad_file = '../example_data/ncdf/CNR4_KSSW_2016142_15min.nc'
 # define scint pair
 # path 12 - BCT -> IMU
 pair_raw = sct.ScintillometerPair(x=[285440.6056, 284562.3107],
-                              y=[5712253.017, 5712935.032],
-                              z_asl=[142, 88],
-                              pair_id='BCT_IMU',
-                              crs='epsg:32631')
+                                  y=[5712253.017, 5712935.032],
+                                  z_asl=[142, 88],
+                                  pair_id='BCT_IMU',
+                                  crs='epsg:32631')
 
 pair = copy.deepcopy(pair_raw)
 
@@ -35,7 +33,6 @@ weightings = (50, 200)
 path_params = {'point_res': point_res,
                'weightings': weightings}
 
-
 spatial_inputs = sct.SpatialInputs(
     domain_size=15000,
     x=float(pair.path_center().x),
@@ -44,7 +41,6 @@ spatial_inputs = sct.SpatialInputs(
     bdsm_path='../example_data/rasters/height_surface_4m.tif',
     cdsm_path='../example_data/rasters/height_veg_4m.tif',
     dem_path='../example_data/rasters/height_terrain_4m.tif')
-
 
 path_transect = pair.path_transect(dsm_file=spatial_inputs.bdsm_path, point_res=10)
 
@@ -55,7 +51,6 @@ zeff = path_transect.effective_beam_height()
 # get estimate of z0 using estimate_z0
 z0_scint = estimate_z0.calculate_quick_z0(spatial_inputs, crop_size=200)['z_0']
 
-
 # retrieve variables needed
 # WX station 15-min file
 # temperature, pressure, wind speed, wind direction
@@ -64,15 +59,15 @@ WX_15min = retrieve_var.retrive_var(wx_file_path_15min,
 # WX station 1-min file
 # wind speed, wind direction
 WX_1min = retrieve_var.retrive_var(wx_file_path_1min,
-                                    ['WS', 'dir'])
+                                   ['WS', 'dir'])
 # scint 15-min file
 # temperature structure parameter
 scint_15min = retrieve_var.retrive_var(scint_path,
-                                    ['CT2'])
+                                       ['CT2'])
 # radiation 15-minute file
 # net all-wave raditation
 rad_15min = retrieve_var.retrive_var(rad_file,
-                                    ['Qstar'])
+                                     ['Qstar'])
 
 # wind calculations
 u_v = wx_u_v_components.wind_components(time=WX_1min['time'],
@@ -93,7 +88,6 @@ WX_hourly = retrieve_var.take_hourly_vars(WX_15min)
 scint_hourly = retrieve_var.take_hourly_vars(scint_15min)
 rad_hourly = retrieve_var.take_hourly_vars(rad_15min)
 
-
 # get stability vars
 stability_vars = wx_stability.wx_stability_vars(zeff=zeff,
                                                 z0_scint=z0_scint,
@@ -107,15 +101,12 @@ hour_inputs = {'wd': WX_hourly['dir'],
                'ustar': stability_vars['ustar'],
                'time': WX_hourly['time']}
 
-
-
 # # for all hours
 # for hour in range(0, 24):
 
-hours_valid = [12]
+hours_valid = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0]
 # for a selection of hours
 for hour in hours_valid:
-
     hour_met_inputs = inputs_at_given_hour.inputs_for_given_hour(hour, hour_inputs)
 
     time = hour_met_inputs['time']
