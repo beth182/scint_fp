d686b8da798d80ec96bf3d4ecb9902d9884da1d7
beth182 <bethany.saunders@pgr.reading.ac.uk>
2021-06-01 11:12:51 +0100

    switch to geopandas for basemap

--- scint_fp/functions/plot_sa_map.py
+++ scint_fp/functions/plot_sa_map.py
@@ -5,33 +5,43 @@
 import rasterio
 import matplotlib.pyplot as plt
 import shapefile as shp  # Requires the pyshp package
+import scintools as sct
+import copy
+from scintools.PointFootprint import _load_rasters
+
+import contextily as cx
+
+import geopandas as gpd
+
 
 
 def plot_map(shpfile_dir):
     """
     Function to produce plots of SAs and maps
     """
+    # ToDo: docstring
 
+    # take first site (BFCL) to define ax
+    df_initial = gpd.read_file(shpfile_dir + 'BFCL.shp')
+    ax = df_initial.plot(edgecolor='k')
+
+    # plotting model grids as shape files:
     # all sites with a unique model grid
-    site_list = ['BFCL', 'BTT', 'KSSW', 'NK', 'RGS', 'SWT']
-
-    plt.figure()
-
+    site_list = ['BTT', 'KSSW', 'NK', 'RGS', 'SWT']
     for site in site_list:
-
         shp_file_path = shpfile_dir + site + '.shp'
+        df = gpd.read_file(shp_file_path)
+        df.plot(edgecolor='k', axes=ax)
 
-        shp_file = shp.Reader(shp_file_path)
-
-        for shape in shp_file.shapeRecords():
-            x = [i[0] for i in shape.shape.points[:]]
-            y = [i[1] for i in shape.shape.points[:]]
-            plt.plot(x, y)
-
+    cx.add_basemap(ax, crs=df.crs)
     plt.show()
+
+    print('end')
+
 
 
 shpfile_dir = 'C:/Users/beths/Desktop/LANDING/UKV_shapefiles/'
+
 plot_map(shpfile_dir)
 
 print('end')
