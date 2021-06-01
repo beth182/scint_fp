67eeab3b6171e3d0fad672a45699b5b0ad48c13f
beth182 <bethany.saunders@pgr.reading.ac.uk>
2021-06-01 11:34:31 +0100

    plot sa

--- scint_fp/functions/plot_sa_map.py
+++ scint_fp/functions/plot_sa_map.py
@@ -2,46 +2,46 @@
 # script to produce maps of source area, model grid & building height's all in one figure
 
 # imports
-import rasterio
 import matplotlib.pyplot as plt
-import shapefile as shp  # Requires the pyshp package
-import scintools as sct
-import copy
-from scintools.PointFootprint import _load_rasters
-
 import contextily as cx
-
 import geopandas as gpd
+import rasterio
+import rasterio.plot
 
 
-
-def plot_map(shpfile_dir):
+def plot_map(shpfile_dir, fp_raster):
     """
     Function to produce plots of SAs and maps
     """
     # ToDo: docstring
 
-    # take first site (BFCL) to define ax
-    df_initial = gpd.read_file(shpfile_dir + 'BFCL.shp')
-    ax = df_initial.plot(edgecolor='k')
+    raster = rasterio.open(fp_raster)
+    fig, ax = plt.subplots()
 
     # plotting model grids as shape files:
     # all sites with a unique model grid
-    site_list = ['BTT', 'KSSW', 'NK', 'RGS', 'SWT']
+    site_list = ['BFCL', 'BTT', 'KSSW', 'NK', 'RGS', 'SWT']
     for site in site_list:
         shp_file_path = shpfile_dir + site + '.shp'
         df = gpd.read_file(shp_file_path)
-        df.plot(edgecolor='k', axes=ax)
+        df.plot(edgecolor='k', ax=ax)
 
     cx.add_basemap(ax, crs=df.crs)
+
+    # use imshow so that we have something to map the colorbar to
+    image_hidden = ax.imshow(raster.read(1))
+    fig.colorbar(image_hidden, ax=ax)
+
+    rasterio.plot.show(raster, ax=ax)
+
     plt.show()
 
     print('end')
 
 
-
 shpfile_dir = 'C:/Users/beths/Desktop/LANDING/UKV_shapefiles/'
+fp_raster = 'C:/Users/beths/Desktop/LANDING/fp_raster_tests/BCT_IMU_15000_2016_142_12.tif'
 
-plot_map(shpfile_dir)
+plot_map(shpfile_dir, fp_raster)
 
 print('end')
