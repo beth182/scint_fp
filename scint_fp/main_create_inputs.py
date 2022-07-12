b9a7cdc81429b35e75397bb766aef6fd141dfb5b
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-07-12 18:24:46 +0100

    simplify - with updates to scint_flux

--- scint_fp/main_create_inputs.py
+++ scint_fp/main_create_inputs.py
@@ -1,39 +1,25 @@
 # Beth Saunders 09/09/2021
 
 # imports
-import scintools as sct
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-from scint_flux import look_up
+from scint_flux import run_function
 
-from scint_flux.functions import find_files
-from scint_flux.functions import read_raw
-from scint_flux.functions import scint_methods
-from scint_flux.functions import time_average
-from scint_flux.functions import wx_data
-from scint_flux.functions import rad_data
-from scint_flux.functions import iterative_stability
-from scint_flux.functions import adj_ws
-from scint_flux.functions import quality_control
-from scint_flux.functions import get_roughness_params
-from scint_flux.functions import benchmark
-
-from scint_fp.functions import estimate_roughness
 from scint_fp.functions import wx_u_v_components
-from scint_fp.functions import retrieve_var
-from scint_fp.functions import inputs_at_given_hour
 from scint_fp.functions import time_average_sa_input
 from scint_fp.functions import sa_creation_selecting
 
 # USER CHOICE
 
-doy_start = 2016176  # CHANGE HERE
-doy_end = 2016176
+# ToDo: be able to do more than one day
+doy = 2016176
+
 # average_period = 10
 average_period = 60
+
 unstable_only = False
+
+pair_id = 'BCT_IMU'
 
 # define site where the radiation data comes from
 rad_site = 'KSSW'
@@ -42,164 +28,33 @@ rad_site = 'KSSW'
 out_dir = 'test_outputs/wd_corrected/'
 # out_dir = 'test_outputs/'
 
-# bdsm_path = 'D:/Documents/large_rasters/clipped/10_m_resampled/resample_10_surface.tif'
-# cdsm_path = 'D:/Documents/large_rasters/clipped/10_m_resampled/resample_10_veg.tif'
-# dem_path = 'D:/Documents/large_rasters/clipped/10_m_resampled/resample_10_terrain.tif'
-
 bdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
 cdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_veg_4m.tif'
 dem_path = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
 
 raw_scint_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_raw/'
 processed_wx_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/new_data_scint/'
-
 # raw_scint_path = 'C:/Users/beths/Desktop/LANDING/data_wifi_problems/'
 # processed_wx_path = 'C:/Users/beths/Desktop/LANDING/data_wifi_problems/'
 
 # bdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_surface_4m.tif'
 # cdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_veg_4m.tif'
 # dem_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_terrain_4m.tif'
-
 # raw_scint_path = '/storage/basic/micromet/Tier_raw/'
 # processed_wx_path = '/storage/basic/micromet/Tier_processing/rv006011/new_data_scint/'
 
-pair_id = 'BCT_IMU'
 
-# SCINT PROPERTIES #####################################################################################################
-# construct path using scintools
-tr_string = pair_id.split('_')[0]
-rx_string = pair_id.split('_')[1]
-
-pair = sct.ScintillometerPair(x=[look_up.site_info[tr_string]['x'], look_up.site_info[rx_string]['x']],
-                              y=[look_up.site_info[tr_string]['y'], look_up.site_info[rx_string]['y']],
-                              z_asl=[look_up.site_info[tr_string]['z_asl'], look_up.site_info[rx_string]['z_asl']],
-                              pair_id=pair_id,
-                              crs='epsg:32631')
-
-roughness_inputs = sct.RoughnessInputs()
-
-point_res = 50
-weightings = (100, 200)
-path_params = {'point_res': point_res,
-               'weightings': weightings}
-
-spatial_inputs = sct.SpatialInputs(
-    domain_size=15000,
-    x=float(pair.path_center().x),
-    y=float(pair.path_center().y),
-    z_asl=pair.path_center_z(),
-    bdsm_path=bdsm_path,
-    cdsm_path=cdsm_path,
-    dem_path=dem_path)
-
-# SCINT DATA ###########################################################################################################
-# find raw scintillometer files
-raw_scint_files = find_files.find_files(doy_start=doy_start, doy_end=doy_end, site=pair.pair_id.split('_')[1],
-                                        instrument='LASMkII_29', level='raw', time_res=None,
-                                        main_dir=raw_scint_path)
-
-# create empty dataframe
-scint_df = pd.DataFrame({'u_cn2': [], 'demod': [], 'sig_u_cn2': [], 'n_samples': []})
-
-for i in range(0, len(raw_scint_files['file_paths'])):
-    # construct dataframe of raw data
-    raw_df = read_raw.read_raw_scint_file(file_path=raw_scint_files['file_paths'][i], date=raw_scint_files['dates'][i])
-
-    # average data to required time resolution
-    av_df = time_average.time_average_voltage(df=raw_df, minute_resolution=1)
-
-    # append to existing dataframe
-    scint_df = scint_df.append(av_df)
-
-# calculate Cn2 using cn2 voltage output and update scint dataframe
-scint_df = scint_methods.cn2_analogue_output(scint_df)
-
-# benchmarking
-# benchmark.test_l0_cn2(scint_df, doy_start, doy_end, pair)
-
-# GET HEIGHTS ##########################################################################################################
-# plot transect
-path_transect = pair.path_transect(dsm_file=spatial_inputs.bdsm_path, point_res=10)
-
-# get effective beam height
-z_fb = path_transect.effective_beam_height()
-
-# ROUGHNESS ESTIMATE ###################################################################################################
-# get estimate of z0 using estimate_z0
-roughness_estimate = estimate_roughness.calculate_quick_roughness(spatial_inputs, crop_size=200)
-
-# add roughness columns to scint_df
-z_d = np.ones(len(scint_df)) * roughness_estimate['z_d']
-z_0 = np.ones(len(scint_df)) * roughness_estimate['z_0']
-scint_df['z_d'] = z_d.tolist()
-scint_df['z_0'] = z_0.tolist()
-
-# calculate effective measurement height
-df = get_roughness_params.calc_z_f(z_fb, scint_df)
-
-# WX DATA ##############################################################################################################
-# find wx files
-wx_files = find_files.find_files(doy_start=doy_start, doy_end=doy_end, site=pair.pair_id.split('_')[0],
-                                 instrument='Davis', level='L2', time_res='1min',
-                                 main_dir=processed_wx_path)
-
-# create new columns
-wx_df = pd.DataFrame({'wind_speed': [], 't_air': [], 'r_h': [], 'pressure': [], 'rain_rate': [], 'z_wx': []})
-
-for i in range(0, len(wx_files['file_paths'])):
-    # process wx
-    process_df = wx_data.create_wx_df(file_path=wx_files['file_paths'][i])
-
-    # append to existing dataframe
-    wx_df = wx_df.append(process_df)
-
-df = wx_data.process_wx_data(df, wx_df)
-
-# SCINT METHODS ########################################################################################################
-
-# Quality control
-df = quality_control.qc_functions(df=df, D=look_up.LAS_mk11_info['aperture_diameter'],
-                                  wavelength=look_up.LAS_mk11_info['wavelength'],
-                                  Lp=pair.path_length())
-
-# calculate structure parameter coefficient and update dataframe
-df = scint_methods.structure_parameter_coefficient(df)
-# calculate the temperature structure parameter and update dataframe
-df = scint_methods.temperature_structure_parameter(df)
-
-# Benchmark
-# benchmark.test_l1_ct2(df, doy_start, doy_end, pair)
-
-# RADIATION FILES ######################################################################################################
-# find radiation files
-rad_files = find_files.find_files(doy_start=doy_start, doy_end=doy_end, site=rad_site,
-                                  instrument='CNR4', level='L0', time_res='5s',
-                                  main_dir=raw_scint_path)
-
-# create empty dataframe
-rad_df = pd.DataFrame({'qstar': [], 'qstar_n_samples': []})
-
-for i in range(0, len(rad_files['file_paths'])):
-    # read radiation files
-    rad_read_df = rad_data.process_rad_data(file_path=rad_files['file_paths'][i])
-
-    # append to existing dataframe
-    rad_df = rad_df.append(rad_read_df)
-
-# add rad_df onto df
-df = df.merge(rad_df, how='outer', left_index=True, right_index=True)
-
-# STABILITY CALCS ######################################################################################################
-
-# using simple method to model QH term using Q*
-df = rad_data.simple_method_qh(df)
-
-# adjust the wind speed at z met sensor to be at zf
-df = adj_ws.adjust_ws_iteratively(df=df, ws=df['wind_speed'], ustar_threshold=0.05, iteration_limit=100,
-                                  neutral_limit=0.03)
-
-# calculate the fluxes
-df = iterative_stability.andreas_flux_calc(df=df, ustar_threshold=0.05, neutral_limit=0.03)
+df = run_function.main_QH_calcs(doy=doy,
+                                pair_id=pair_id,
+                                sa_res=average_period,
+                                raw_scint_path=raw_scint_path,
+                                processed_wx_path=processed_wx_path,
+                                bdsm_path=bdsm_path,
+                                cdsm_path=cdsm_path,
+                                dem_path=dem_path,
+                                las_instrument_type='LASMkII_29',
+                                sa_path=False,
+                                write_file=False)
 
 # wind calculations
 component_df = wx_u_v_components.ws_wd_to_u_v(df['wind_speed_adj'], df['wind_direction_corrected'])
@@ -239,6 +94,6 @@ elif average_period == 60:
     else:
         csv_out_string = 'met_inputs_hourly_all_stab_'
 
-df_selection.to_csv(out_dir + csv_out_string + str(doy_start)[-3:] + '.csv')
+df_selection.to_csv(out_dir + csv_out_string + str(doy)[-3:] + '.csv')
 
 print('END')
