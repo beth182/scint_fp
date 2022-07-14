a9bee072591ac4ac936ed9e8399c370fed9745d9
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-07-14 15:34:54 +0100

    multiple days run

--- scint_fp/main_create_inputs.py
+++ scint_fp/main_create_inputs.py
@@ -3,9 +3,11 @@
 # imports
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 
 from scint_flux import run_function
 from scint_flux.functions import plots
+from scint_flux.functions import benchmark
 
 from scint_fp.functions import wx_u_v_components
 from scint_fp.functions import time_average_sa_input
@@ -14,7 +16,8 @@ from scint_fp.functions import sa_creation_selecting
 # USER CHOICE
 
 # ToDo: be able to do more than one day
-doy = 2017054
+DOY_start = 2016176
+DOY_stop = 2016176
 
 # average_period = 10
 average_period = 60
@@ -45,60 +48,85 @@ processed_wx_path = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_pr
 # raw_scint_path = '/storage/basic/micromet/Tier_raw/'
 # processed_wx_path = '/storage/basic/micromet/Tier_processing/rv006011/new_data_scint/'
 
-
-df = run_function.main_QH_calcs(doy=doy,
-                                pair_id=pair_id,
-                                sa_res=average_period,
-                                raw_scint_path=raw_scint_path,
-                                processed_wx_path=processed_wx_path,
-                                bdsm_path=bdsm_path,
-                                cdsm_path=cdsm_path,
-                                dem_path=dem_path,
-                                las_instrument_type='LASMkII_29',
-                                sa_path=False,
-                                write_file=False)
-
-
-# plots.plot_qh(df)
-
-# wind calculations
-component_df = wx_u_v_components.ws_wd_to_u_v(df['wind_speed_adj'], df['wind_direction_corrected'])
-df = pd.concat([df, component_df], axis=1)
-
-# take the last 10 minute averages and calculate the standard deviation of wind for that period
-df_av = time_average_sa_input.time_average_sa(df, average_period)
-
-# convert the averages of the u and the v component back to wind speed and direction
-av_comp = wx_u_v_components.u_v_to_ws_wd(df_av['u_component'], df_av['v_component'])
-df_av = pd.concat([df_av, av_comp], axis=1)
-
-# determine which times are made into source areas here:
 ########################################################################################################################
-# take 10-minute average values only ending on the hour
-# df_selection = retrieve_var.take_hourly_vars(df_av)
-df_selection = df_av
 
-# find unstable times only
+start_dt = dt.datetime.strptime(str(DOY_start), '%Y%j')
+stop_dt = dt.datetime.strptime(str(DOY_stop), '%Y%j')
 
-if unstable_only:
-    df_selection = sa_creation_selecting.find_unstable_times(df_selection, neutral_limit=0.03)
+delta = stop_dt - start_dt
 
-# remove nan rows
-df_selection = sa_creation_selecting.remove_nan_rows(df_selection)
+doy_list = []
+for i in range(delta.days + 1):
+    day = start_dt + dt.timedelta(days=i)
+    doy_list.append(day.strftime('%Y%j'))
 
-########################################################################################################################
-# save to csv
-if average_period == 10:
+df_list = []
+
+for doy in doy_list:
+
+    df = run_function.main_QH_calcs(doy=doy,
+                                    pair_id=pair_id,
+                                    sa_res=average_period,
+                                    raw_scint_path=raw_scint_path,
+                                    processed_wx_path=processed_wx_path,
+                                    bdsm_path=bdsm_path,
+                                    cdsm_path=cdsm_path,
+                                    dem_path=dem_path,
+                                    las_instrument_type='LASMkII_29',
+                                    sa_path=False,
+                                    write_file=False)
+
+    # plots.plot_qh(df)
+
+    # APPEND BEFORE AVERAGING
+    df_list.append(df)
+
+    # wind calculations
+    component_df = wx_u_v_components.ws_wd_to_u_v(df['wind_speed_adj'], df['wind_direction_corrected'])
+    df = pd.concat([df, component_df], axis=1)
+
+    # take the last 10 minute averages and calculate the standard deviation of wind for that period
+    df_av = time_average_sa_input.time_average_sa(df, average_period)
+
+    # convert the averages of the u and the v component back to wind speed and direction
+    av_comp = wx_u_v_components.u_v_to_ws_wd(df_av['u_component'], df_av['v_component'])
+    df_av = pd.concat([df_av, av_comp], axis=1)
+
+    # determine which times are made into source areas here:
+    ########################################################################################################################
+    # take 10-minute average values only ending on the hour
+    # df_selection = retrieve_var.take_hourly_vars(df_av)
+    df_selection = df_av
+
+    # find unstable times only
+
     if unstable_only:
-        csv_out_string = 'met_inputs_minutes_'
-    else:
-        csv_out_string = 'met_inputs_minutes_all_stab_'
-elif average_period == 60:
-    if unstable_only:
-        csv_out_string = 'met_inputs_hourly_'
-    else:
-        csv_out_string = 'met_inputs_hourly_all_stab_'
+        df_selection = sa_creation_selecting.find_unstable_times(df_selection, neutral_limit=0.03)
 
-df_selection.to_csv(out_dir + csv_out_string + str(doy)[-3:] + '.csv')
+    # remove nan rows
+    df_selection = sa_creation_selecting.remove_nan_rows(df_selection)
+
+    ########################################################################################################################
+    # save to csv
+    if average_period == 10:
+        if unstable_only:
+            csv_out_string = 'met_inputs_minutes_'
+        else:
+            csv_out_string = 'met_inputs_minutes_all_stab_'
+    elif average_period == 60:
+        if unstable_only:
+            csv_out_string = 'met_inputs_hourly_'
+        else:
+            csv_out_string = 'met_inputs_hourly_all_stab_'
+
+    # df_selection.to_csv(out_dir + csv_out_string + str(doy)[-3:] + '.csv')
+
+df_all = pd.concat(df_list)
+
+plots.plot_qh(df_all)
+
+pair = run_function.construct_path(pair_id, bdsm_path, cdsm_path, dem_path)['pair']
+
+benchmark.test_l1_qh(df_all, DOY_start, DOY_stop, pair)
 
 print('END')
