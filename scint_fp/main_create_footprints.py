76f385258cd0a10914b21d88b4c8e94fc1025cbd
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-07-22 18:28:14 +0100

    read csv to make fp

--- scint_fp/main_create_footprints.py
+++ scint_fp/main_create_footprints.py
@@ -4,73 +4,9 @@ import numpy as np
 
 from scint_flux import look_up
 
-# CHANGE HERE
-# LOCAL
-# out_dir = 'test_outputs/wd_corrected/'
-# bdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
-# cdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_veg_4m.tif'
-# dem_path = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
-
-# CLUSTER
-out_dir = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/'
-bdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_surface_4m.tif'
-cdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_veg_4m.tif'
-dem_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_terrain_4m.tif'
-
-
-
-
-
-
-
-
-# read a csv with DOY inputs to run the SA model
-csv_path = './DOY_in.csv'
-DOY_in_df = pd.read_csv(csv_path)
-DOY_in_df['DOY_format'] = (DOY_in_df.Year.astype(str) + DOY_in_df.DOY.astype(str)).astype(int)
-doy_list = list(DOY_in_df['DOY_format'])
-
-df_list = []
-
-for doy in doy_list:
-
-
-    csv_name = 'met_inputs_hourly_176.csv'  # CHANGE HERE
-    print(csv_name)
-
-
-
-pair_id = 'BCT_IMU'
-# construct path using scintools
-tr_string = pair_id.split('_')[0]
-rx_string = pair_id.split('_')[1]
-
-pair = sct.ScintillometerPair(x=[look_up.site_info[tr_string]['x'], look_up.site_info[rx_string]['x']],
-                              y=[look_up.site_info[tr_string]['y'], look_up.site_info[rx_string]['y']],
-                              z_asl=[look_up.site_info[tr_string]['z_asl'], look_up.site_info[rx_string]['z_asl']],
-                              pair_id=pair_id,
-                              crs='epsg:32631')
-
-roughness_inputs = sct.RoughnessInputs()
-
-spatial_inputs = sct.SpatialInputs(
-    domain_size=15000,
-    x=float(pair.path_center().x),
-    y=float(pair.path_center().y),
-    z_asl=pair.path_center_z(),
-    bdsm_path=bdsm_path,
-    cdsm_path=cdsm_path,
-    dem_path=dem_path)
-
-point_res = 50
-weightings = (100, 200)
-path_params = {'point_res': point_res,
-               'weightings': weightings}
-
 
 def create_footprints(pair, roughness_inputs, spatial_inputs, path_params,
                       out_dir, csv_name):
-
     # read inputs csv
     df = pd.read_csv(out_dir + csv_name)
     df.rename(columns={'Unnamed: 0': 'time'}, inplace=True)
@@ -81,7 +17,6 @@ def create_footprints(pair, roughness_inputs, spatial_inputs, path_params,
 
     # create footprint for each entry in dataframe
     for index, row in df.iterrows():
-
         time = row.name
         sigv = row['sig_v']
         wd = row['wind_direction_convert']
@@ -112,19 +47,113 @@ def create_footprints(pair, roughness_inputs, spatial_inputs, path_params,
         fp_path.footprint[fp_path.footprint == 0.0] = np.nan
 
         string_to_save = str(pair.pair_id) + '_' + str(spatial_inputs.domain_size) + '_' + title_string
-        # CHANGE HERE
-        file_out = out_dir + 'hourly/' + string_to_save + '.tif'
-        fp_path.save(out_dir + 'hourly/' + string_to_save)
+        file_out = out_dir + string_to_save + '.tif'
+        fp_path.save(out_dir + string_to_save)
         fp_path.save_tiff(file_out)
 
         print(title_string)
 
         print('END')
 
+
+out_dir_base = 'test_outputs/'
+# days read in from csv
+csv_path = './DOY_in.csv'
+DOY_in_df = pd.read_csv(csv_path)
+
+df_list = []
+
+for index, row in DOY_in_df.iterrows():
+
+    doy = int(str(row.Year) + str(row.DOY))
+
+    run_location = row.run_location
+    if run_location == 'cluster':
+        bdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_surface_4m.tif'
+        cdsm_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_veg_4m.tif'
+        dem_path = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintools/example_inputs/rasters/height_terrain_4m.tif'
+    elif run_location == 'local':
+        bdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
+        cdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_veg_4m.tif'
+        dem_path = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
+    elif run_location == 'mount':
+        bdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_surface_4m.tif'
+        cdsm_path = 'D:/Documents/scintools/example_inputs/rasters/height_veg_4m.tif'
+        dem_path = 'D:/Documents/scintools/example_inputs/rasters/height_terrain_4m.tif'
+
+    average_period = row.average
+
+    if row.mins_ending_10 == 1:
+        mins_ending_10 = True
+    else:
+        mins_ending_10 = False
+
+    if row.unstable_only == 1:
+        unstable_only = True
+    else:
+        unstable_only = False
+
+    # construct csv name based on imputs
+    if average_period == 10:
+        if unstable_only:
+            if mins_ending_10:
+                average_period_string = 'minutes_ending'
+            else:
+                average_period_string = 'minutes'
+        else:
+            if mins_ending_10:
+                average_period_string = 'minutes_all_stab_ending'
+            else:
+                average_period_string = 'minutes_all_stab'
+    elif average_period == 60:
+        if unstable_only:
+            average_period_string = 'hourly'
+        else:
+            average_period_string = 'hourly_all_stab'
+
+    csv_name = 'met_inputs_' + average_period_string + '_' + str(doy)[4:] + '.csv'
+
+    # construct out_dir based on inputs
+    if average_period == 10:
+        if mins_ending_10:
+            out_string = '10_mins_ending'
+        else:
+            out_string = '10_mins'
+    elif average_period == 60:
+        out_string = 'hourly'
+
+    out_dir = out_dir_base + out_string + '/'
+
+    pair_id = row.pair
+    # construct path using scintools
+    tr_string = pair_id.split('_')[0]
+    rx_string = pair_id.split('_')[1]
+
+    pair = sct.ScintillometerPair(x=[look_up.site_info[tr_string]['x'], look_up.site_info[rx_string]['x']],
+                                  y=[look_up.site_info[tr_string]['y'], look_up.site_info[rx_string]['y']],
+                                  z_asl=[look_up.site_info[tr_string]['z_asl'], look_up.site_info[rx_string]['z_asl']],
+                                  pair_id=pair_id,
+                                  crs='epsg:32631')
+
+    roughness_inputs = sct.RoughnessInputs()
+
+    spatial_inputs = sct.SpatialInputs(
+        domain_size=15000,
+        x=float(pair.path_center().x),
+        y=float(pair.path_center().y),
+        z_asl=pair.path_center_z(),
+        bdsm_path=bdsm_path,
+        cdsm_path=cdsm_path,
+        dem_path=dem_path)
+
+    point_res = 50
+    weightings = (100, 200)
+    path_params = {'point_res': point_res,
+                   'weightings': weightings}
+
+    create_footprints(pair, roughness_inputs, spatial_inputs, path_params,
+                      out_dir, csv_name)
+
     print('END')
-
-
-create_footprints(pair, roughness_inputs, spatial_inputs, path_params,
-                  out_dir, csv_name)
 
 print('end')
