e86e977623c0d104ef366a677ac99f3c8af9e546
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-07-25 09:54:29 +0100

    csv name fix

--- scint_fp/main_create_footprints.py
+++ scint_fp/main_create_footprints.py
@@ -114,7 +114,7 @@ for index, row in DOY_in_df.iterrows():
         else:
             average_period_string = 'hourly_all_stab'
 
-    csv_name = 'met_inputs_' + average_period_string + '_' + str(doy)[4:] + '.csv'
+    csv_name = 'met_inputs_' + average_period_string + '_' + doy_string + '.csv'
 
     # construct out_dir based on inputs
     if average_period == 10:
