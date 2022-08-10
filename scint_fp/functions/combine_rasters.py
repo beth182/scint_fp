d8b49f3e4d526161fda6d256bf4184302e9161cd
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-08-10 13:32:54 +0100

    fix

--- scint_fp/functions/combine_rasters.py
+++ scint_fp/functions/combine_rasters.py
@@ -7,14 +7,14 @@ import pandas as pd
 
 path_string = 'BCT_IMU'
 
-main_dir = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
-# main_dir = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
+# main_dir = '//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
+main_dir = '/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
 
 out_dir = './'
 
 csv_path = '../DOY_in.csv'
 DOY_in_df = pd.read_csv(csv_path)
-DOY_in_df['doy_string'] = DOY_in_df.Year.astype(str) + DOY_in_df.DOY.astype(str)
+DOY_in_df['doy_string'] = DOY_in_df.Year.astype(str) + DOY_in_df.DOY.astype(str).str.zfill(3)
 DOYlist = DOY_in_df['doy_string'].astype(int).to_list()
 
 file_list = []
