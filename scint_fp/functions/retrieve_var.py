ed041978f6fb156cc9a665f8cf059e037b837891
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-12-19 18:33:37 +0000

    function moved to scint_flux

--- scint_fp/functions/retrieve_var.py
+++ scint_fp/functions/retrieve_var.py
@@ -1,40 +1,4 @@
 # Beth Saunders 27/05/21
-# read var from netcdf
-
-import netCDF4 as nc
-import numpy as np
-
-
-def retrive_var(file_path,
-                var_names):
-    """
-    Retrive variable from ncdf file
-    :param file_path: sting of ncdf filepath
-    :param var_names: LIST of strings; variable names as they appear in ncdf file
-    :return: Dict of both time (as array of datetime objects) & variable array
-    """
-
-    ncdf_file = nc.Dataset(file_path)
-
-    # reads in observation height
-    height_obs = ncdf_file.variables['height']
-    z_davis = height_obs[:][0]
-
-    file_time = ncdf_file.variables['time']
-    time_dt = nc.num2date(file_time[:], file_time.units)
-
-    var_dict = {'time': time_dt}
-
-    z_davis_arr = np.ones(len(file_time)) * z_davis
-
-    var_dict['z_wx'] = z_davis_arr
-
-    for var_name in var_names:
-        file_var = ncdf_file.variables[var_name]
-        var_array = np.array(file_var[:, 0, 0, 0], dtype=np.float)
-        var_dict[var_name] = var_array
-
-    return var_dict
 
 
 def take_hourly_vars(df):
