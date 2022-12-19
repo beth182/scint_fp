3485721704c7bffed97d95544fcc4a283feef9bf
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-12-19 15:42:09 +0000

    rename fun

--- scint_fp/functions/combine_rasters/combine_rasters.py
+++ scint_fp/functions/combine_rasters/combine_rasters.py
@@ -5,13 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # from scint_fp.functions import landcover_type_present as lc
 
 
-def combine_rasters(path_string,
-                    main_dir='//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/',
-                    # main_dir='/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
-                    ):
+def combine_sa_rasters(path_string,
+                       main_dir='//rdg-home.ad.rdg.ac.uk/research-nfs/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/',
+                       # main_dir='/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/test_outputs/10_mins_ending/'
+                       ):
     """
     Takes a list of days (from csv, e.g. DOY_in) and combine each source area of that day (all times) into one
     source area - representing a "climatology" source area of the given target period (days included).
@@ -67,11 +68,8 @@ def combine_rasters(path_string,
         dst.write_band(1, raster_array.astype(rasterio.float32))
 
 
-
 # combine_rasters('IMU_BTT')
 print('end')
-
-
 
 # optional post-evaluation
 
@@ -91,8 +89,6 @@ P13_df_sa_data = [[P13_sa_df.loc[1]['sa_weight_percent'],
 P13_sa_df = pd.DataFrame(P13_df_sa_data, columns=df_sa_columns)
 P13_sa_df.index = ['IMU_BTT']
 
-
-
 P15_file = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combine_rasters/SCT_SWT.tif'
 P15_sa_df = lc.landcover_fractions_in_SA_weighted(P15_file)
 
@@ -106,8 +102,6 @@ P15_df_sa_data = [[P15_sa_df.loc[1]['sa_weight_percent'],
 
 P15_sa_df = pd.DataFrame(P15_df_sa_data, columns=df_sa_columns)
 P15_sa_df.index = ['SCT_SWT']
-
-
 
 P11_file = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combine_rasters/BTT_BCT.tif'
 P11_sa_df = lc.landcover_fractions_in_SA_weighted(P11_file)
@@ -126,7 +120,6 @@ P11_sa_df.index = ['BTT_BCT']
 P12_file = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combine_rasters/BCT_IMU.tif'
 P12_sa_df = lc.landcover_fractions_in_SA_weighted(P12_file)
 
-
 P12_df_sa_data = [[P12_sa_df.loc[1]['sa_weight_percent'],
                    P12_sa_df.loc[2]['sa_weight_percent'],
                    P12_sa_df.loc[3]['sa_weight_percent'],
@@ -138,11 +131,7 @@ P12_df_sa_data = [[P12_sa_df.loc[1]['sa_weight_percent'],
 P12_sa_df = pd.DataFrame(P12_df_sa_data, columns=df_sa_columns)
 P12_sa_df.index = ['BCT_IMU']
 
-
-
-
 combine = pd.concat([P13_sa_df, P12_sa_df, P11_sa_df, P15_sa_df])
-
 
 color_list = ["dimgrey", "lightgrey", "deepskyblue", "lawngreen", "darkgreen", "limegreen", "olive"]
 
@@ -150,16 +139,12 @@ fig, ax = plt.subplots(figsize=(12, 12))
 
 combine.plot(ax=ax, kind='bar', stacked=True, color=color_list, width=0.85)
 
-
 plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
 ax.set_ylim(0, 100)
 ax.set_xlim(-.5, len(combine.index) - 0.5)
 
-
-
 plt.savefig('C:/Users/beths/OneDrive - University of Reading/Paper 1/mask_tests/output.png', bbox_inches='tight')
 # plt.show()
-
 
 
 print('end')
