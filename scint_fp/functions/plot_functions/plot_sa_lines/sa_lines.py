3f1f77480b0af376ab339125bdd6be69a43d1f0e
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-12-20 14:19:47 +0000

    if name == main

--- scint_fp/functions/plot_functions/plot_sa_lines/sa_lines.py
+++ scint_fp/functions/plot_functions/plot_sa_lines/sa_lines.py
@@ -1,15 +1,15 @@
-import contextily as cx
 import rasterio.plot
 import numpy as np
-import matplotlib.pyplot as plt
-import glob
-import os
 import geopandas as gpd
 import matplotlib.colors as colors
 import matplotlib as mpl
 import matplotlib.lines as mlines
+import os
+import matplotlib.pyplot as plt
 
 from scintools.PointFootprint import trim_fp
+
+import scint_fp.functions.plot_functions.plot_sa_lines.sa_lines_funs as sa_lines
 
 mpl.rcParams.update({'font.size': 15})  # updating the matplotlib fontsize
 
@@ -180,4 +180,16 @@ colour_list = ['green', 'blue', 'red', 'magenta']
 
 plot_sa_lines(file_list=file_list_outside, colour_list=colour_list)
 
-print('end')
+
+if __name__ == '__main__':
+
+    doy_choice = 123
+    sa_dir = 'C:/Users/beths/OneDrive - University of Reading/local_runs_data/fp_output/' + str(doy_choice) + '/hourly/'
+
+    save_path = os.getcwd().replace('\\', '/') + '/'
+
+    file_list = sa_lines.find_SA_rasters(sa_main_dir=sa_dir)
+    colour_list = sa_lines.get_colours(cmap=plt.cm.inferno, file_list=file_list)
+    sa_lines.plot_sa_lines(file_list=file_list, colour_list=colour_list, doy_choice=doy_choice, save_path=save_path)
+
+    print('end')
