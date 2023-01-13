008936b67769fe0181582ffba79e57c2d56b47ed
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-01-13 18:19:42 +0000

    add lc by sector fun and tests with main

--- scint_fp/functions/sa_lc_fractions/lc_by_sector/lc_by_sector.py
+++ scint_fp/functions/sa_lc_fractions/lc_by_sector/lc_by_sector.py
@@ -10,52 +10,43 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-save_path = os.getcwd().replace('\\', '/') + '/'
 
-# read in lc fraction file
-# ToDo: move this file
-landcover_location = 'C:/Users/beths/OneDrive - University of Reading/Model_Eval/QGIS/Elliott/LandUseMM_7classes_32631.tif'
+def lc_by_sector(image,
+                 transform,
+                 save_path,
+                 landcover_location='C:/Users/beths/OneDrive - University of Reading/Model_Eval/QGIS/Elliott/LandUseMM_7classes_32631.tif'):
+    print('end')
 
+    # crop square extent of landcover fractions file to the same as the
+    maskDs = gdal.Open(SA_file, GA_ReadOnly)  # your mask raster
+    projection = maskDs.GetProjectionRef()
+    geoTransform = maskDs.GetGeoTransform()
+    minx = geoTransform[0]
+    maxy = geoTransform[3]
+    maxx = minx + geoTransform[1] * maskDs.RasterXSize
+    miny = maxy + geoTransform[5] * maskDs.RasterYSize
+    data = gdal.Open(landcover_location, GA_ReadOnly)
 
-# read in SA file
-# ToDo: temp file, replace to be dynamic in function
-# example SA raster:
-SA_file = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combine_rasters/BCT_IMU.tif'
-sa_raster = rasterio.open(SA_file)
-sa_array = sa_raster.read(1)
+    # Your data the one you want to clip
+    output = save_path + 'output.tif'  # output file
+    gdal.Translate(output, data, format='GTiff', projWin=[minx, maxy, maxx, miny], outputSRS=projection)
 
-# crop square extent of landcover fractions file to the same as the
-maskDs = gdal.Open(SA_file, GA_ReadOnly)  # your mask raster
-projection = maskDs.GetProjectionRef()
-geoTransform = maskDs.GetGeoTransform()
-minx = geoTransform[0]
-maxy = geoTransform[3]
-maxx = minx + geoTransform[1] * maskDs.RasterXSize
-miny = maxy + geoTransform[5] * maskDs.RasterYSize
-data = gdal.Open(landcover_location, GA_ReadOnly)
+    # reads this cropped dataset
+    landcover_crop = rasterio.open(output)
+    landcover_array = landcover_crop.read(1)
 
-# Your data the one you want to clip
-output = save_path + 'output.tif'  # output file
-gdal.Translate(output, data, format='GTiff', projWin=[minx, maxy, maxx, miny], outputSRS=projection)
-
-# reads this cropped dataset
-landcover_crop = rasterio.open(output)
-landcover_array = landcover_crop.read(1)
-
-# test of two arrays have the same shape
-if sa_array.shape == landcover_array.shape:
-    pass
-else:
-    if landcover_array.shape == sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 2].shape:
-        sa_array = sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 2]
-    elif landcover_array.shape == sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 1].shape:
-        # remove last row/col
-        sa_array = sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 1]
+    # test of two arrays have the same shape
+    if sa_array.shape == landcover_array.shape:
+        pass
     else:
-        assert sa_array.shape == landcover_array.shape
+        if landcover_array.shape == sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 2].shape:
+            sa_array = sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 2]
+        elif landcover_array.shape == sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 1].shape:
+            # remove last row/col
+            sa_array = sa_array[0:sa_array.shape[0] - 1, 0:sa_array.shape[1] - 1]
+        else:
+            assert sa_array.shape == landcover_array.shape
 
+    mask = np.ma.masked_where(np.isnan(sa_array), landcover_array, copy=True)
 
-mask = np.ma.masked_where(np.isnan(sa_array), landcover_array, copy=True)
-
-print('end')
-
+    print('end')
--- scint_fp/functions/sa_lc_fractions/lc_by_sector/main_lc_by_sector.py
+++ scint_fp/functions/sa_lc_fractions/lc_by_sector/main_lc_by_sector.py
@@ -2,12 +2,16 @@
 # split landcover dataset into sectors by angle and calculate land cover fraction
 
 # imports
+import os
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 
 import scintools as sct
 
 from scint_fp.functions.sa_lc_fractions.lc_by_sector import mask_SA_by_sector
+from scint_fp.functions.sa_lc_fractions.lc_by_sector import lc_by_sector
+
+save_path = os.getcwd().replace('\\', '/') + '/'
 
 # define a centre point (in this case, the centre of the path)
 # path 12 - BCT -> IMU
@@ -22,16 +26,19 @@ raster_filepath = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combi
 
 sector_dict = mask_SA_by_sector.mask_raster_by_sector(raster_filepath, centre_point)
 
-
+# sanity check
+"""
 fig, ax = plt.subplots()
-
 for key in sector_dict['images'].keys():
-
     ax.imshow(sector_dict['images'][key][0])
+"""
 
+# test file - sector 1
+test_image = sector_dict['images'][1]
+test_transform = sector_dict['transforms'][1]
 
-
+lc_by_sector.lc_by_sector(image=test_image,
+                          transform=test_transform,
+                          save_path=save_path)
 
 print('end')
-
-# plt.imshow(sector_dict['images'][1][0])
