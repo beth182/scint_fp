bdfbfd39e6e651a219197daa73142e80abb43b4b
beth182 <bethany.saunders@pgr.reading.ac.uk>
2023-01-16 18:11:13 +0000

    use existing lines to get df to plot

--- scint_fp/functions/sa_lc_fractions/lc_by_sector/main_lc_by_sector.py
+++ scint_fp/functions/sa_lc_fractions/lc_by_sector/main_lc_by_sector.py
@@ -3,6 +3,7 @@
 
 # imports
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from scint_fp.functions.sa_lc_fractions import lc_fractions_in_sa
@@ -30,9 +31,34 @@ raster_filepath = 'C:/Users/beths/OneDrive - University of Reading/Paper 2/combi
 mask_SA_by_sector.mask_raster_by_sector(raster_filepath, centre_point, save_path)
 """
 
-# landcover function
-# ToDo: currently manually feeding first raster as test. Need to make this a loop
-df = lc_fractions_in_sa.landcover_fractions_in_SA_weighted(sa_tif_path=save_path + 'sector_rasters/1.tif',
-                                                           save_path=save_path)
+# landcover functions in each sector - weighted by SA
+
+
+sa_sector_dir = save_path + 'sector_rasters/'
+
+df = pd.DataFrame()
+
+for file in os.listdir(sa_sector_dir):
+    file_path = sa_sector_dir + file
+
+    sa_df = lc_fractions_in_sa.landcover_fractions_in_SA_weighted(sa_tif_path=file_path,
+                                                               save_path=save_path)
+
+    # getting df in correct format for bar plot
+    df_sa_columns = ['Building', 'Impervious', 'Water', 'Grass', 'Deciduous', 'Evergreen', 'Shrub']
+    df_sa_data = [[sa_df.loc[1]['sa_weight_percent'],
+                   sa_df.loc[2]['sa_weight_percent'],
+                   sa_df.loc[3]['sa_weight_percent'],
+                   sa_df.loc[4]['sa_weight_percent'],
+                   sa_df.loc[5]['sa_weight_percent'],
+                   sa_df.loc[6]['sa_weight_percent'],
+                   sa_df.loc[7]['sa_weight_percent']]]
+    # a dataframe of one source area
+    df_sa = pd.DataFrame(df_sa_data, columns=df_sa_columns)
+    df_sa.index = [int(file.split('.')[0])]  # index as sector number
+
+    df = df.append(df_sa)
+
+df = df.sort_index()
 
 print('end')
