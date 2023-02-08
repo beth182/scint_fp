aa1a873c43e2b6fcd49949afe3f0a7cf2a1782d1
Micromet <$MM_MICRO_EMAIL>
2023-02-08 13:56:04 +0000

    add input shell from cluster

---
+++ scint_fp/create_input_csvs/input_submit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=5G 
+
+here=/storage/basic/micromet/Tier_processing/rv006011/PycharmProjects/scintillometer_footprints/scint_fp/
+
+SCRIPT=main_create_inputs.py
+
+cd $here
+
+module load anaconda
+source activate rv006011
+
+python $SCRIPT > $SCRIPT.log 2>$SCRIPT.2.log
